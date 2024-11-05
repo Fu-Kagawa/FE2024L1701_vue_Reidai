1968c9aeecf94dc1b31f00ae172b8da5a78516ec
SD2C_香川文紀 <kagawa-f@s.asojuku.ac.jp>
2024-11-05 17:30:31 +0900

    update02

--- Reidai/script/script.js
+++ Reidai/script/script.js
@@ -1,18 +1,13 @@
 // JavaScript用ファイル
 new Vue({
     el: '#app',
+    
     data: {
-      password: '',
-      confirmPassword: ''
     },
     computed: {
       // パスワードが一致しているかを確認する算出プロパティ
-      passwordMismatch() {
-        return this.password && this.confirmPassword && this.password !== this.confirmPassword;
-      },
-      // 登録ボタンを有効にするかどうか
-      canRegister() {
-        return this.password && this.confirmPassword && !this.passwordMismatch;
-      }
+
+      // 登録ボタンを有効にするかどうかの算出プロパティ
+
     }
   });
